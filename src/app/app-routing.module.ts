5c24599e6f4c26ccbf6daeb1a47c1574e45317af
pastorvio10 <cristibas10@hotmail.com>
2023-01-01 21:05:50 -0500

    fin de la seccion 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,31 +2,16 @@ import { NgModule } from '@angular/core';
 //este comomModule me perimite usar ngif ngfor ngelse
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagesfoundComponent } from './pages/nopagesfound/nopagesfound.component';
-import { PagesComponent } from './pages/pages.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+
+import { NopagesfoundComponent } from './nopagesfound/nopagesfound.component';
+import { AuthRoutingModule } from './auth/auth.routing';
 
 const routes: Routes = [
   //cuando se va al app-component se establece el router oulet y me redirecciona a pages component 
-    {
-      path: '',component: PagesComponent,
-      //para crear las rutas hijas le decimos que son hijas de pagescomponent
-      children: [
-        {path: 'dashboard',component: DashboardComponent},
-        {path: 'progress',component: ProgressComponent},
-        {path: 'grafica1',component: Grafica1Component},  
-        {path: '',redirectTo: '/dashboard', pathMatch: 'full'}, 
-      ]
-    },
+    
    
-    {path: 'login',component: LoginComponent},
-    {path: 'register',component: RegisterComponent},
-   
-   
+    {path: '',redirectTo: '/dashboard', pathMatch: 'full'}, 
     {path: '**', component:NopagesfoundComponent}
 
 ]
@@ -35,7 +20,9 @@ const routes: Routes = [
   declarations: [],
   imports: [
     //forRoot es para rutas principales y forchilde es apra subrutas o rutas hijas
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [RouterModule]
 })
